1c08045e200ff372168bdca97ffcb668eea44403
kaustubh <kaustubh229@gmail.com>
2020-09-24 02:48:35 +0530

    hbs, css, express added

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
+const express = require('express')
+const path = require('path');
+const hbs = require('hbs');
 const apiCall = require('./utils/api-call');
 const api = require('./utils/api-call');
 
-let location = 'lucknow';
+const app = express();
 
-api(process.argv[2].split(' ').join('-'), (response) => {
-    console.log(response)
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        'title': 'Homepage',
+        'author': 'Kaustubh Mishra'
+    })
 })
+
+app.get('/api', (req, res) => {
+    res.render('api', {
+        'title': 'API Page',
+        'author': 'Kaustubh Mishra'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('details', {
+        'title': 'About Us',
+        'author': 'Kaustubh Mishra'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        'title': '404 Error',
+        'author': 'Kaustubh Mishra'
+    })
+})
+// let location = 'lucknow';
+
+// api(process.argv[2].split(' ').join('-'), (response) => {
+//     console.log(response)
+// })
+
+app.listen(3000)
